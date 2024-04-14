4798117dd27d2cf3a8648402942f3c1742708fd6
Raj Patil <rjpatil53@gmail.com>
2024-04-14 16:32:57 +0530

    Tina integration

---
+++ tina/config.ts
@@ -0,0 +1,106 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "blog",
+        label: "Blogs",
+        path: "content/blogs",
+        format: "mdx",
+        fields: [
+          {
+            type: "string",
+            name: "slug",
+            label: "Slug",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+            required: true,
+            templates: [
+              {
+                name: "HeaderLink",
+                label: "HeaderLink",
+                fields: [
+                  {
+                    name: "children",
+                    label: "children",
+                    type: "rich-text",
+                  },
+                  {
+                    name: "href",
+                    label: "href",
+                    type: "string",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            type: "datetime",
+            name: "pubDate",
+            label: "Published date",
+            required: true,
+            ui: {
+              dateFormat: "YY-MM-DD",
+            },
+          },
+          {
+            type: "datetime",
+            name: "updatedDate",
+            label: "Updated date",
+            ui: {
+              dateFormat: "YY-MM-DD",
+            },
+          },
+          {
+            type: "image",
+            name: "heroImage",
+            label: "Hero image",
+          },
+        ],
+      },
+    ],
+  },
+});
